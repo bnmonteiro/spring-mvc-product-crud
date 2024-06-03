c1ad7527cc8a45e0c3d4a62731f07b822f2f816d
bnmonteiro <bnmonteiro2@gmail.com>
2024-06-03 18:19:12 -0300

    add msg in log fail. logout button and reader user role

--- src/main/java/com/spring/mvc/product/crud/SecurityConfig.java
+++ src/main/java/com/spring/mvc/product/crud/SecurityConfig.java
@@ -16,20 +16,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("admin")
-            .password(passwordEncoder().encode("!123@"))
-            .roles("SUPERUSER");
+                .withUser("admin")
+                .password(passwordEncoder().encode("@123"))
+                .roles("SUPERUSER")
+                .and()
+                .withUser("reader")
+                .password(passwordEncoder().encode("@456"))
+                .roles("READERUSER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/products/**").hasRole("SUPERUSER")
-            .anyRequest().authenticated()
+//            .antMatchers("/products/**").hasRole("SUPERUSER")
+                .antMatchers("/products").hasAnyRole("SUPERUSER", "READERUSER")
+                .antMatchers("/products/new", "/products/edit/**", "/products/delete/**").hasRole("SUPERUSER")
+
+                .anyRequest().authenticated()
             .and()
             .formLogin()
             .loginPage("/login")
             .permitAll()
+            .defaultSuccessUrl("/products", true) // Redirect to "/products" after successful login
             .and()
             .logout()
             .permitAll();
